cdb786fee364f559fa2447de05f305ec46bcc32a
Patrick Kim <patrick.yrkim@gmail.com>
2023-01-25 23:39:00 -0800

    Add profile column component

---
+++ src/components/LogoSearch/LogoSearch.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const LogoSearch = () => {
+  return (
+    <div className="LogoSearch">
+        LogoSearch
+    </div>
+  )
+}
+
+export default LogoSearch
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+
 import './Home.css'
+import ProfileSide from '../../components/profile/ProfileSide'
 
 const Home = () => {
   return (
     <div className='Home'>
-        <div className='profileSide'>Profile</div>
+        <ProfileSide />
         <div className='postSide'>Posts</div>
         <div className='rightSide'>Right</div>
     </div>
