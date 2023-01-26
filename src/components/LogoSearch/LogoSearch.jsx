cad6ddf55c51d7d73908751df58dd2efc50be018
Patrick Kim <patrick.yrkim@gmail.com>
2023-01-25 23:55:41 -0800

    Add search styling

--- src/components/LogoSearch/LogoSearch.jsx
+++ src/components/LogoSearch/LogoSearch.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import { UilSearch } from '@iconscout/react-unicons'
+
+import './LogoSearch.css'
+import Logo from '../../img/logo.png'
 
 const LogoSearch = () => {
   return (
     <div className="LogoSearch">
-        LogoSearch
+        <img src={Logo} alt="" />
+        <div className="Search">
+            <input type="text" placeholder="#Explore" />
+            <div className="s-icon">
+                <UilSearch />
+            </div>
+        </div>
     </div>
   )
 }
