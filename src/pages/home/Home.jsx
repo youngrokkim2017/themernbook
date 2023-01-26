83ec3ca1c46ac76c74097a794301447d7d0f0991
Patrick Kim <patrick.yrkim@gmail.com>
2023-01-25 23:21:59 -0800

    Add Home page

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Home.css'
+
+const Home = () => {
+  return (
+    <div className='Home'>
+        <div className='profileSide'>Profile</div>
+        <div className='postSide'>Posts</div>
+        <div className='rightSide'>Right</div>
+    </div>
+  )
+}
+
+export default Home
