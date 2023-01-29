aad3805d9edc845e974b8b0e97c20cb903004ed7
Patrick Kim <patrick.yrkim@gmail.com>
2023-01-28 22:36:27 -0800

    Add post side component

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,12 +2,13 @@ import React from 'react'
 
 import './Home.css'
 import ProfileSide from '../../components/ProfileSide/ProfileSide'
+import PostSide from '../../components/PostSide/PostSide'
 
 const Home = () => {
   return (
     <div className='Home'>
         <ProfileSide />
-        <div className='postSide'>Posts</div>
+        <PostSide />
         <div className='rightSide'>Right</div>
     </div>
   )
