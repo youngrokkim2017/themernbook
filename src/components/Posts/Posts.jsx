4175841baa1596a794bfcf312b927a893b8c7549
Patrick Kim <patrick.yrkim@gmail.com>
2023-01-31 02:00:07 -0800

    Add posts component

---
+++ src/components/Posts/Posts.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import './Posts.css'
+import { PostsData } from '../../data/PostsData'
+import Post from '../Post/Post'
+
+const Posts = () => {
+  return (
+    <div className="Posts">
+        {PostsData.map((post, id) => (
+            <Post data={post} id={id}/>
+        ))}
+    </div>
+  )
+}
+
+export default Posts
