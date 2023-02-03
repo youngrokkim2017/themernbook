066932783a707ebac9f09674fc61a79942ad1ef6
Patrick Kim <patrick.yrkim@gmail.com>
2023-02-02 18:09:44 -0800

    Add profile page

---
+++ frontend/src/pages/Profile/Profile.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+import './Profile.css'
+import PostSide from '../../components/PostSide/PostSide'
+import ProfileCard from '../../components/ProfileCard/ProfileCard'
+import ProfileLeft from '../../components/ProfileLeft/ProfileLeft'
+import RightSide from '../../components/RightSide/RightSide'
+
+const Profile = () => {
+  return (
+    <div className="Profile">
+      <ProfileLeft />
+      <div className="Profile-Center">
+        <ProfileCard />
+        <PostSide />
+      </div>
+      <RightSide />
+    </div>
+  )
+}
+
+export default Profile
