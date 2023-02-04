c18629443bf49cacb461038aa367096e54aec2b2
Patrick Kim <patrick.yrkim@gmail.com>
2023-02-03 23:44:22 -0800

    Add auth page

---
+++ frontend/src/pages/Auth/Auth.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+import './Auth.css'
+
+const Auth = () => {
+  return (
+    <div className="Auth">
+      <div className="a-left">
+        <img src={Logo} alt="" />
+        <div className="Webname">
+          <h1>Social Media</h1>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Auth
