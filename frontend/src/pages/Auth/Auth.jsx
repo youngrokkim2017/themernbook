898cb200805cd318cf83b726c53c2fa75ac2fa50
Patrick Kim <patrick.yrkim@gmail.com>
2023-02-03 23:52:01 -0800

    Add signup and login features

--- frontend/src/pages/Auth/Auth.jsx
+++ frontend/src/pages/Auth/Auth.jsx
@@ -1,6 +1,85 @@
 import React from 'react'
 
 import './Auth.css'
+import Logo from '../../img/logo.png'
+
+const SignUp = () => (
+    <div className="a-right">
+    <form className="infoForm authForm">
+      <h3>Sign up</h3>
+      <div>
+        <input
+          type="text"
+          placeholder="First Name"
+          className="infoInput"
+          name="firstname"
+        />
+        <input
+          type="text"
+          placeholder="Last Name"
+          className="infoInput"
+          name="lastname"
+        />
+      </div>
+      <div>
+        <input
+          type="text"
+          className="infoInput"
+          name="username"
+          placeholder="Usernames"
+        />
+      </div>
+      <div>
+        <input
+          type="text"
+          className="infoInput"
+          name="password"
+          placeholder="Password"
+        />
+        <input
+          type="text"
+          className="infoInput"
+          name="confirmpass"
+          placeholder="Confirm Password"
+        />
+      </div>
+      <div>
+          <span style={{fontSize: '12px'}}>Already have an account. Login!</span>
+      </div>
+      <button className="button infoButton" type="submit">Signup</button>
+    </form>
+  </div>
+)
+
+const LogIn = () => (
+    <div className="a-right">
+    <form className="infoForm authForm">
+      <h3>Log In</h3>
+      <div>
+        <input
+          type="text"
+          placeholder="Username"
+          className="infoInput"
+          name="username"
+        />
+      </div>
+      <div>
+        <input
+          type="password"
+          className="infoInput"
+          placeholder="Password"
+          name="password"
+        />
+      </div>
+      <div>
+          <span style={{ fontSize: "12px" }}>
+            Don't have an account Sign up
+          </span>
+        <button className="button infoButton">Login</button>
+      </div>
+    </form>
+  </div>
+)
 
 const Auth = () => {
   return (
@@ -11,6 +90,8 @@ const Auth = () => {
           <h1>Social Media</h1>
         </div>
       </div>
+      {/* <LogIn /> */}
+      <SignUp />
     </div>
   )
 }
