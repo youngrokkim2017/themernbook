977ae72e383f235d50d39dd2de362a32fcd45575
Patrick Kim <patrick.yrkim@gmail.com>
2023-02-04 23:52:57 -0800

    Add share modal

--- frontend/src/components/RightSide/RightSide.jsx
+++ frontend/src/components/RightSide/RightSide.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { UilSetting } from '@iconscout/react-unicons'
 
 import './RightSide.css'
@@ -6,10 +6,13 @@ import Home from "../../img/home.png"
 import Notification from "../../img/noti.png"
 import Comment from "../../img/comment.png"
 import TrendCard from '../TrendCard/TrendCard'
+import ShareModal from '../ShareModal/ShareModal'
 
 const RightSide = () => {
+  const [modalOpened, setModalOpened] = useState(false)
+
   return (
-    <div className="Rightside">
+    <div className="RightSide">
       <div className="navIcons">
         <img src={Home} alt="" />
         <UilSetting />
@@ -17,9 +20,13 @@ const RightSide = () => {
         <img src={Comment} alt="" />
       </div>
       <TrendCard />
-      <button className="button right-button">
+      <button className="button right-button" onClick={() => setModalOpened(true)}>
         Share
       </button>
+      <ShareModal 
+        modalOpened={modalOpened}
+        setModalOpened={setModalOpened}
+      />
     </div>
   )
 }
