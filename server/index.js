59efef801d057b7c9cb45c87b70d3dee09a015c9
Patrick Kim <patrick.yrkim@gmail.com>
2023-02-06 21:45:57 -0800

    Add auth route

--- server/index.js
+++ server/index.js
@@ -3,12 +3,14 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 
+import AuthRoute from './routes/AuthRoute.js';
+
 dotenv.config();
 
 const app = express();
 
+// middleware
 app.use(bodyParser.json({ limit: '30mb', extended: true }))
-
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 
 mongoose
@@ -21,3 +23,6 @@ mongoose
     )
     .then(() => app.listen(process.env.PORT, () => console.log(`listening to port ${process.env.PORT}`)))
     .catch((error) => console.log(error))
+
+// routes
+app.use('/auth', AuthRoute)
--- server/routes/AuthRoute.js
+++ server/routes/AuthRoute.js
@@ -1,9 +1,13 @@
 import express from 'express';
 
+import { registerUser } from '../controllers/AuthController.js';
+
 const router = express.Router();
 
-router.get('/', async (req, res) => {
-    res.send('Auth Route')
-})
+// router.get('/', async (req, res) => {
+//     res.send('Auth Route')
+// })
+
+router.post('/register', registerUser)
 
 export default router
