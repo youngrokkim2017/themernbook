1f773a6216b73eb979f62395c4df7e03c79d7a8d
Patrick Kim <patrick.yrkim@gmail.com>
2023-02-06 21:45:37 -0800

    Add auth controllers

---
+++ server/controllers/AuthController.js
@@ -0,0 +1,14 @@
+import UserModel from "../models/User.js";
+
+export const registerUser = async (req, res) => {
+    const { username, password, firstname, lastname } = req.body;
+
+    const newUser = new UserModel({ username, password, firstname, lastname });
+
+    try {
+        await newUser.save();
+        res.status(200).json(newUser);
+    } catch (error) {
+        res.status(500).json({ message: error.message })
+    }
+}
