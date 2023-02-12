1b496fd2177b8a74629b3c2446859379f9f2ca92
Patrick Kim <patrick.yrkim@gmail.com>
2023-02-12 12:30:56 -0800

    Update register variables

--- server/controllers/AuthController.js
+++ server/controllers/AuthController.js
@@ -3,19 +3,31 @@ import bcrypt from 'bcrypt';
 import UserModel from "../models/User.js";
 
 export const registerUser = async (req, res) => {
-    const { username, password, firstname, lastname } = req.body;
+    // const { username, password, firstname, lastname } = req.body;
 
     const salt = await bcrypt.genSalt(10);  // amount of hashing the viewing string
-    const hashedPassword = await bcrypt.hash(password, salt);
+    // const hashedPassword = await bcrypt.hash(password, salt);
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
 
-    const newUser = new UserModel({ 
-        username, 
-        password: hashedPassword, 
-        firstname, 
-        lastname,
-    });
+    // const newUser = new UserModel({ 
+    //     username, 
+    //     password: hashedPassword, 
+    //     firstname, 
+    //     lastname,
+    // });
+
+    req.body.password = hashedPassword;
+
+    const newUser = new UserModel(req.body);
+    const { username } = req.body;
 
     try {
+        // check if username already exists
+        const oldUser = await UserModel.findOne({ username })
+        if (oldUser) {
+            return res.status(400).json({ message: 'username is already registered' })
+        }
+
         await newUser.save();
         res.status(200).json(newUser);
     } catch (error) {
