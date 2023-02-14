595642fa5dee337a35653f044275d797331635ea
Patrick Kim <patrick.yrkim@gmail.com>
2023-02-13 22:43:14 -0800

    Add react router dom

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';  //v6
 
+import App from './App';
 import store from './store/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="*" element={<App />} />
+      </Routes>
+    </BrowserRouter>
   </Provider>
 );
