58f5346cbbcb6eaaf36ccbf91269c35b1c112e43
Patrick Kim <patrick.yrkim@gmail.com>
2023-02-17 23:05:48 -0800

    Update file names

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,9 +2,9 @@ import { Routes, Route, Navigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 
 import './App.css';
-import Auth from './pages/Auth/Auth';
+import Auth from './pages/auth/Auth';
 import Home from './pages/home/Home';
-import Profile from './pages/Profile/Profile';
+import Profile from './pages/profile/Profile';
 
 function App() {
   const user = useSelector((state) => state.authReducer.authData);
--- frontend/src/actions/uploadAction.js
+++ frontend/src/actions/uploadAction.js
@@ -1,4 +1,4 @@
-import * as UploadApi from '../api/UploadRequest';
+import * as UploadApi from '../api/uploadRequest';
 
 export const uploadImage = (data) => async (dispatch) => {
     try {
