abf64ce69f7cfb4950dcbc0652295a0920fa486a
Patrick Kim <patrick.yrkim@gmail.com>
2023-02-19 23:37:32 -0800

    Add jwt middleware

--- server/routes/UserRoute.js
+++ server/routes/UserRoute.js
@@ -1,14 +1,15 @@
 import express from 'express';
 
 import { deleteUser, followUser, getAllUsers, getUser, unfollowUser, updateUser } from '../controllers/UserController.js';
+import authMiddleware from '../middleware/authMiddleware.js';
 
 const router = express.Router();
 
 router.get('/', getAllUsers);
 router.get('/:id', getUser);
-router.put('/:id', updateUser);
-router.delete('/:id', deleteUser);
-router.put('/:id/follow', followUser);
-router.put('/:id/unfollow', unfollowUser);
+router.put('/:id', authMiddleware, updateUser);
+router.delete('/:id', authMiddleware, deleteUser);
+router.put('/:id/follow', authMiddleware, followUser);
+router.put('/:id/unfollow', authMiddleware, unfollowUser);
 
 export default router;
