798478493c43ba3e283c0553809f4bc1028b3418
Patrick Kim <patrick.yrkim@gmail.com>
2023-03-07 01:04:44 -0800

    Add findChat

--- client/src/api/chatRequest.js
+++ client/src/api/chatRequest.js
@@ -5,3 +5,5 @@ const API = axios.create({ baseURL: "http://localhost:5000" });
 export const userChats = (id) => API.get(`/chat/${id}`)
 
 export const createChat = (data) => API.post('/chat/', data)
+
+export const findChat = (firstId, secondId) => API.get(`/chat/find/${firstId}/${secondId}`)
