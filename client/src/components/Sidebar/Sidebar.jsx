a4ff0b4670f1d9236c21f1e69e91529bb1c8d913
Patrick Kim <patrick.yrkim@gmail.com>
2023-03-31 23:27:27 -0700

    Update sidebar component

--- client/src/components/Sidebar/Sidebar.jsx
+++ client/src/components/Sidebar/Sidebar.jsx
@@ -1,39 +1,54 @@
-import React, { useState } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { Link } from 'react-router-dom'
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import React, { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
 
-import './Sidebar.css'
-import { logout } from '../../actions/authAction'
+import './Sidebar.css';
+import { logout } from '../../actions/authAction';
 
 const Sidebar = () => {
   const user = useSelector((state) => state.authReducer.authData);
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
   const dispatch = useDispatch();
+  const [isNavOpen, setIsNavOpen] = useState(false)
 
-  const toggleSidebar = () => {
-    setIsSidebarOpen(!isSidebarOpen)
-  }
+  useEffect(() => {
+    const handleResize = () => {
+        if (window.innerWidth < 600 && isNavOpen) {
+            setIsNavOpen(false)
+        }
+    }
+
+    window.addEventListener("resize", handleResize)
+    return () => window.removeEventListener("resize", handleResize)
+  }, [isNavOpen])
 
   const handleLogout = () => {
     dispatch(logout())
   }
 
   return (
-    <div className="Sidebar">
-       <button onClick={toggleSidebar}>
-         <i class="fa-solid fa-bars"></i>
+    <div className='sidebar-component'>
+      <button className="sidebar-toggle" onClick={() => setIsNavOpen(!isNavOpen)}>
+        <span class="material-symbols-outlined">
+          {isNavOpen ? "toggle_on" : "toggle_off"}
+        </span>
       </button>
-      <div className={isSidebarOpen === true ? 'open-sidebar' : 'hidden-sidebar'}>
-        <span className='sidebar-title'>Menu</span>
-        <nav className='sidebar-nav-item'>
-          <Link to='../home' style={{ textDecoration: 'none' }}>Home</Link>
-          <Link to={`/profile/${user._id}`}>My Profile</Link>
-          <Link to='../chat'>Chat</Link>
-          <button onClick={handleLogout}>Logout</button>
-        </nav>
-      </div>
+      <nav className={`nav ${isNavOpen ? "nav-open" : "nav-closed"}`}>
+        <div className="logo">logo</div>
+        <ul>
+          <li>
+            <Link className='links' to='../home' style={{ textDecoration: 'none' }}>Home</Link>
+          </li>
+          <li>
+            <Link className='links' to={`/profile/${user._id}`}>My Profile</Link>
+          </li>
+          <li>
+            <Link className='links' to='../chat'>Chat</Link>
+          </li>
+          <li>
+            <button className='links' onClick={handleLogout} style={{ border: 'none', width: '100%', textAlign: 'left' }}>Logout</button>
+          </li>
+        </ul>
+      </nav>
     </div>
   )
 }
@@ -41,7 +56,52 @@ const Sidebar = () => {
 export default Sidebar
 
 
-// WITH useRef
+// WITH USESTATE
+
+// import React, { useState } from 'react'
+// import { useDispatch, useSelector } from 'react-redux'
+// import { Link } from 'react-router-dom'
+// // import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+// import './Sidebar.css'
+// import { logout } from '../../actions/authAction'
+
+// const Sidebar = () => {
+//   const user = useSelector((state) => state.authReducer.authData);
+//   const [isLoggedIn, setIsLoggedIn] = useState(false);
+//   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+//   const dispatch = useDispatch();
+
+//   const toggleSidebar = () => {
+//     setIsSidebarOpen(!isSidebarOpen)
+//   }
+
+//   const handleLogout = () => {
+//     dispatch(logout())
+//   }
+
+//   return (
+//     <div className="Sidebar">
+//        <button onClick={toggleSidebar}>
+//          <i class="fa-solid fa-bars"></i>
+//       </button>
+//       <div className={isSidebarOpen === true ? 'open-sidebar' : 'hidden-sidebar'}>
+//         <span className='sidebar-title'>Menu</span>
+//         <nav className='sidebar-nav-item'>
+//           <Link to='../home' style={{ textDecoration: 'none' }}>Home</Link>
+//           <Link to={`/profile/${user._id}`}>My Profile</Link>
+//           <Link to='../chat'>Chat</Link>
+//           <button onClick={handleLogout}>Logout</button>
+//         </nav>
+//       </div>
+//     </div>
+//   )
+// }
+
+// export default Sidebar
+
+
+// WITH USEREF
 
 // import React, { useState, useRef } from 'react'
 // import { useDispatch, useSelector } from 'react-redux'
